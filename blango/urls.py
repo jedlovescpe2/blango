fae3ce402b4f2f6ed9eb287659b4dcaa31cefefe
Jed Llenado <jedlovescpe2@gmail.com>
2022-06-21 22:08:14 +0000

    Finish django configuration

--- blango/urls.py
+++ blango/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from django.conf import settings
+# print(f"Time zone: {settings.TIME_ZONE}")
 
 import blog.views
 
